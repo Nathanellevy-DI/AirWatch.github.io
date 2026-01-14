82c180e4b8c31b21a3e36fbeb14dec8c8bfeb430
nathanellevy-di <nathanellevydi@gmail.com>
2026-01-14 20:25:44 +0200

    Fix: Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/AirWatch/',
+  base: '/AirWatch.github.io/',
   plugins: [
     react(),
     VitePWA({
